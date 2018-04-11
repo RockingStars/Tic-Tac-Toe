9b5008249a8cfa15cf844ad70949702a32393aa2
Robert Monden <dev@robertmonden.com>
2018-04-11 23:54:33 +0200

    Fixed Tic-Tac-Toe

--- controllers/TTTController.java
+++ controllers/TTTController.java
@@ -23,8 +23,6 @@ public class TTTController extends AbstractGame {
     public TTTController(Player player1, Player player2) {
         super(player1, player2);
 
-        startGame();
-
         this.player1.setCharacter('x');
         this.player2.setCharacter('o');
 
@@ -49,86 +47,87 @@ public class TTTController extends AbstractGame {
             if (_model.isValidMove(x, y)) {
                 if (yourTurn) {
                     CommandExecutor.execute(new MoveCommand(ServerConnection.getInstance(), y * 3 + x));
-                    _model.setPlayerAtPosition(currentPlayer, x, y);
+
+                    _model.setPlayerAtPosition(player1, x, y);
                     _view.setCellImage(x, y);
+
+                    _view.setStatus("Opponent's turn");
                     yourTurn = false;
-                    setCurrentPlayer(1);
-                } else {
-                    _view.setErrorStatus("It's not your turn");
                 }
-            } else
+                else
+                    _view.setErrorStatus("It's not your turn");
+            }
+            else
                 _view.setErrorStatus("Invalid move");
-        } else{
-            gameEnded();
         }
+        else
+            gameEnded();
+
         /*if (currentPlayer == player2) {
             randomGenerator();
         }*/
     }
 
+    @Override
+    public void doPlayerMove(int position) {
+        if (!gameFinished()) {
+            if (yourTurn)
+                return;
+
+            int x = position % 3;
+            int y = position / 3;
+
+            _model.setPlayerAtPosition(player2, x, y);
+            _view.setCellImage(x, y);
+
+            yourTurn = true;
+        }
+    }
+
     public void randomGenerator() {
         Random rand = new Random();
+
         int tempX = 3;
         int tempY = 3;
+
         while (!_model.isValidMove(tempX, tempY)) {
             tempX = rand.nextInt(2 + 1);
             tempY = rand.nextInt(2 + 1);
         }
+
         doPlayerMove(tempX, tempY);
-
     }
 
     @Override
-    public void doPlayerMove(int position) {
-        if (!gameFinished()) {
-            if (yourTurn) {
-                return;
-            }
-            int x = position % 3;
-            int y = position / 3;
-
-            _model.setPlayerAtPosition(currentPlayer, x, y);
-            _view.setCellImage(x, y);
-            setCurrentPlayer(0);
-        }
-    }
+    public void showPossibleMoves() {}
 
     @Override
-    public void setCurrentPlayer(int id) {
-        if (currentState == State.GAME_FINISHED)
-            return;
-
-        currentPlayer = id == 0 ? player1 : player2;
-        _view.setStatus(_model.getTurnMessage(currentPlayer));
+    public void doYourTurn() {
+        _view.setStatus("It's your turn!");
+        yourTurn = true;
     }
 
-    @Override
-    public void showPossibleMoves() {
-        return;
-    }
-
-    public boolean gameFinished(){
+    private boolean gameFinished() {
         if (_model.hasWon(player1) || _model.hasWon(player2)) {
             _view.setStatus("Player " + (yourTurn ? player1 : player2).getUsername() + " has won! Congratulations.");
+
             setGameState(State.GAME_FINISHED);
             _view.setIsFinished(true);
+
             return true;
         }
-        else if(_model.isFull()){
-            return true;
-        } else {
-            return false;
-        }
+
+        return _model.isFull();
     }
 
-    public void gameEnded(){
+    public void gameEnded() {
         super.gameEnded();
         _view.setIsFinished(true);
-        if(!_model.isFull()) {
+
+        if(!_model.isFull())
             _view.setStatus("Player " + (yourTurn ? player1 : player2).getUsername() + " has won! Congratulations.");
-        } else {
+        else
             _view.setStatus("It's a draw! N00bs");
-        }
 
         setGameState(State.GAME_FINISHED);
 
@@ -140,9 +139,8 @@ public class TTTController extends AbstractGame {
 
             returnToLobby.showAndWait();
 
-            if (returnToLobby.getResult() == ButtonType.OK) {
+            if (returnToLobby.getResult() == ButtonType.OK)
                 toLobby();
-            }
         });
     }
 }
